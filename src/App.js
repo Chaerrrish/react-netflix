c1d21fe4805f569941bd4130ebe9eb224521a6f1
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-03-17 21:44:17 +0900

    ~3.17 finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import {Outlet, Routes, Route} from "react-router-dom"
 import './App.css';
-import Banner from './components/Banner.js';
 import Nav from './components/Nav.js'
+import Footer from "./components/Footer.js"
+import DetailPage from './pages/DetailPage/index.js';
+import SearchPage from './pages/SearchPage/index.js';
+import MainPage from "./pages/MainPage/index.js";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Banner />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import "./Nav.css"
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
     const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -17,6 +20,11 @@ export default function Nav() {
         };
     }, []);
 
+    const handleChange = (e) => {
+      setSearchValue(e.target.value);
+      navigate(`/search?q=${e.target.value}`)
+    };
+    
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
@@ -25,6 +33,13 @@ export default function Nav() {
         className="nav__logo"
         onClick={() => window.location.reload()}
       />
+      <input 
+        value={searchValue} 
+        onChange={handleChange} 
+        className="nav__input"
+        type="text"
+        placeholder='영화를 검색해주세요.'
+        />
       <img
         alt="User logged"
         src="https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg"
