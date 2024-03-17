b198cc615fab75c64b9aa514a031277044a16ebe
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-03-17 22:07:11 +0900

    modified

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='react-netflix'>
     <App />
   </BrowserRouter>
 );
@@ -16,3 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
